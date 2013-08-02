f9180e575007c489d06ae7db588e5515f899491c
László Monda <laci@monda.hu>
2013-08-02 16:10:54 +0200

    Some minor cleanups.

--- get-current-window.c
+++ get-current-window.c
@@ -11,34 +11,31 @@ long unsigned int get_float_property(unsigned char *property)
 
 int main(int argc, char** argv)
 {
-    Display *display;
     char *display_name = NULL;  // could be the value of $DISPLAY
-
-    Atom *atoms, *atom;
+    Display *display;
+    Atom *atoms, *atom, actual_type, filter_atom;
     int count, i;
-    Atom actual_type;
     int actual_format;
+    int status;
     unsigned long nitems;
     unsigned long bytes_after;
     unsigned char *prop;
-    int status;
-    Atom filter_atom;
 
     display = XOpenDisplay(display_name);
     if (display == NULL) {
         fprintf (stderr, "%s:  unable to open display '%s'\n", argv[0], XDisplayName (display_name));
     }
     int screen = XDefaultScreen(display);
-    long unsigned int root_window = RootWindow(display, screen);
+    unsigned long root_window = RootWindow(display, screen);
 
     filter_atom = XInternAtom(display, "_NET_ACTIVE_WINDOW", True);
     status = XGetWindowProperty(display, root_window, filter_atom, 0, MAXSTR, False, AnyPropertyType, &actual_type, &actual_format, &nitems, &bytes_after, &prop);
-    long unsigned int application_window = get_float_property(prop);
+    unsigned long application_window = get_float_property(prop);
 
     atoms = XListProperties(display, application_window, &count);
 
-    for (i = 0; i < count; i++) {
-        atom = atoms+i;
+    for (i=0; i<count; i++) {
+        atom = atoms + i;
         status = XGetWindowProperty(display, application_window, *atom, 0, MAXSTR, False, AnyPropertyType, &actual_type, &actual_format, &nitems, &bytes_after, &prop);
         if (status == BadWindow) {
             printf("window id # 0x%lx does not exists!", application_window);
